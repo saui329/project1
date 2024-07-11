6736e1defabfbf0c83ab87c4fe4b769a5c2b9c28
64660 <646600893@qq.com>
2024-07-11 21:32:29 +0800

    订单统计、销量统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,8 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -38,7 +40,6 @@ public class ReportController {
         return Result.success(reportService.getTurnoverStatistics(begin,end));
     }
 
-    //用户统计
     @GetMapping("/userStatistics")
     @ApiOperation("用户统计")
     public Result<UserReportVO> userStatistics(
@@ -46,6 +47,24 @@ public class ReportController {
             @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end){
         log.info("用户数据统计：{}，{}",begin,end);
         return Result.success(reportService.getUserStatistics(begin,end));
+    }
+
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end) {
+        log.info("订单数据统计：{},{}", begin, end);
+        return Result.success(reportService.getOrderStatistics(begin, end));
+    }
+
+    @GetMapping("/top10")
+    @ApiOperation("销量排名top10")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end){
+        log.info("销量排名top10：{},{}",begin,end);
+        return Result.success(reportService.getSalesTop10(begin,end));
     }
 
 
